d29dded4363dbc5a2f5c95d09ab58ad67985c1bf
Mingxia Zeng <emiliezeng91@gmail.com>
2024-09-12 18:24:28 -0400

    fix: replace image urls to the current server location

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
