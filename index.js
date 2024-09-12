d515f7796633f78c912bb4627feec37f785e0046
Maria Lukowich <mlukowich27@gmail.com>
2024-09-12 13:38:03 -0600

    adding basic code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// make routes in folder
+import express from "express";
+import cors from "cors";
+import knex from "knex";
+import config from "./knexfile.js";
+import "dotenv/config";
+
+const app = express();
+const { PORT, CORS_ORIGIN } = process.env;
+
+
+app.use(express.json()); 
+app.use(express.static("public")); 
+app.use(cors({ origin: CORS_ORIGIN })); 
+
+// app.use("/api", route); 
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
