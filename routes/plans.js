496cf61cbd80b31e0a13121a0567ed4a9eaaeb02
Mingxia Zeng <emiliezeng91@gmail.com>
2024-09-12 19:42:41 -0400

    feat: add request validation

--- routes/plans.js
+++ routes/plans.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { validatePlanInput } from "../middlewares/validation.js";
+import { logRequest } from "../middlewares/log.js";
 import { getRecommendation } from "../utils/recommendation.js";
 import dotenv from "dotenv";
 
@@ -7,27 +8,31 @@ dotenv.config();
 
 const router = express.Router();
 
-router.post("/", validatePlanInput, (req, res) => {
+router.post("/", logRequest, validatePlanInput, (req, res) => {
+  try {
     const { budget, family_members } = req.body;
     const recommendation = getRecommendation(budget, family_members);
     const { streaming_services, theme_packs } = recommendation;
     replaceThemePackImageUrls(theme_packs);
     replaceStreamingServiceImageUrls(streaming_services);
     res.json({ streaming_services, theme_packs });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const replaceThemePackImageUrls = (theme_packs) => {
-    theme_packs.forEach(theme_pack => {
-        theme_pack.channels.forEach(channel => {
-            channel.image = replaceImageUrl(channel.image);
-        });
+  theme_packs.forEach((theme_pack) => {
+    theme_pack.channels.forEach((channel) => {
+      channel.image = replaceImageUrl(channel.image);
     });
+  });
 };
 
 const replaceStreamingServiceImageUrls = (streaming_services) => {
-    streaming_services.forEach(service => {
-        service.image = replaceImageUrl(service.image);
-    });
+  streaming_services.forEach((service) => {
+    service.image = replaceImageUrl(service.image);
+  });
 };
 
 export default router;
