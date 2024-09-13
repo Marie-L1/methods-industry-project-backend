325f32f1685318dfe3bd392908052ecffe0ecd9c
Mingxia Zeng <emiliezeng91@gmail.com>
2024-09-13 12:01:32 -0400

    port to 8080

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import plansRouter from "./routes/plans.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
